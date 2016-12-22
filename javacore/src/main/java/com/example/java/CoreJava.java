e178e2d1190947c2a3063dca2ffde50909f364c2
rana <ranabid90@gmail.com>
2016-12-22 05:55:00 +0530

    Initial checkin to git
    Change-Id: Ic123a37cb3b1ac6b7a73937a87f2d91baa977c4b
    Signed-off-by: rana <ranabid90@gmail.com>

---
+++ javacore/src/main/java/com/example/java/CoreJava.java
@@ -0,0 +1,11 @@
+package com.example.java;
+
+public class CoreJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+
+	}
+
+}
